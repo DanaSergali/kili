ba215ac8400495209344f813ef04d68f2ca2d3ae
DanaSergali <32065017+DanaSergali@users.noreply.github.com>
2017-12-06 19:01:14 +0300

    Update python.py

--- hw5/python.py
+++ hw5/python.py
@@ -2,12 +2,10 @@ with open("text.txt", "w", encoding="utf-8") as f:
     number = []
     for i in range(7):
         number.append(input("Введите, пожалуйста, число: "))
-        
     for i in range(7):
         for m in range(int(number[i])):
             f.write("X")
         f.write("\n")
-        
 with open("text.txt", encoding="utf-8") as f:
     text = f.read()
     print(text)
