9cd5cab7089ad0dad282943ec92149bf5cdf8ecf
DanaSergali <32065017+DanaSergali@users.noreply.github.com>
2018-02-01 21:56:41 +0300

    Add files via upload

---
+++ bonus/4.py
@@ -0,0 +1,8 @@
+print("Pig Latin это языковая игра..")
+
+letters=set("ЁУЕЫАОЭЯИЮёуеыаоэяию")
+word=input("Введите слово или фразу, и прога переведет его на PIG LATIN: ")
+for letter in letters:
+    word=word.replace(letter,letter+"c"+letter)
+
+print(word)    
