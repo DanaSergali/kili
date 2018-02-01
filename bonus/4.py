9d3e33fecef2441b8d62e8833c0174d787b2bb11
DanaSergali <32065017+DanaSergali@users.noreply.github.com>
2018-02-01 22:00:05 +0300

    Update 4.py

--- bonus/4.py
+++ bonus/4.py
@@ -1,7 +1,7 @@
-print("Pig Latin это языковая игра..")
+print("Кирпичный язык это..")
 
 letters=set("ЁУЕЫАОЭЯИЮёуеыаоэяию")
-word=input("Введите слово или фразу, и прога переведет его на PIG LATIN: ")
+word=input("Введите слово или фразу, и прога переведет его на «кирпичный язык: ")
 for letter in letters:
     word=word.replace(letter,letter+"c"+letter)
 #replace заменяет все вхождения одной строки на другую
